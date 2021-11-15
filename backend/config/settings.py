1f48be40c8950e1cc4b342ca5c59152870d5edb0
Oshae Alexis <terongp3@gmail.com>
2021-11-14 22:22:48 -0500

    Added Files

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -97,7 +97,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
     }
-
+#
 # DATABASES = {
 #     'default': {
 #        'ENGINE': 'django.db.backends.postgresql',
